00237387cf8dae156b1527dbc285c4c16c6726dd
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-10-22 23:09:18 +0530

    carddesign

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Media Query and Bootstrap");
+
+//Media Query(RWD)-Responsive Web Design
+
+//For each device we have different type of screen capacity by using 
+//this we can able to acheive the responsiveness of the screen resolution
+
+//Syntax:
+//@media screen and (max width:upto 700px)
+//{CSSCode goes here}
+
+// Bootstrap 4.6 Version we are using
+
+
+//CDN LINK Content Delivery Network Link which is used to give on Head tag
+
+//<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
